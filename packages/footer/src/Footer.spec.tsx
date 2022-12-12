92f06c62eac530ee4ea25dc1ddcb792be50518ae
Sergey Melnik <sergey.melnik@valor-software.com>
2022-12-12 12:17:16 +0100

    testbuild

--- packages/footer/src/Footer.spec.tsx
+++ packages/footer/src/Footer.spec.tsx
@@ -7,6 +7,7 @@ import { render } from '@testing-library/react';
 import { Footer } from './Footer';
 
 
+
 test('renders footer', () => {
   const { getByText } = render(<Footer />);
   const linkElement = getByText(/footer/i);
